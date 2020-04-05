d3af9102ea1cd197b6ff0b55feef0012c2d9b141
taraskiiin <taraskravets95@gmail.com>
2020-04-06 02:17:45 +0300

    Updates

---
+++ precache-manifest.683c931ddc500f29a2d9ac463324a804.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca77e62b5e5ed53cecc188742980ba0e",
+    "url": "/react-cursor-2hw/index.html"
+  },
+  {
+    "revision": "a74fc9f5b2329f251252",
+    "url": "/react-cursor-2hw/static/css/main.9346ddb9.chunk.css"
+  },
+  {
+    "revision": "ce9ed882bfb41706b5d5",
+    "url": "/react-cursor-2hw/static/js/2.5330f1d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cursor-2hw/static/js/2.5330f1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74fc9f5b2329f251252",
+    "url": "/react-cursor-2hw/static/js/main.bc12b342.chunk.js"
+  },
+  {
+    "revision": "9a28522de6e8328b7fb3",
+    "url": "/react-cursor-2hw/static/js/runtime-main.33b8c25d.js"
+  },
+  {
+    "revision": "ae10377bd9e0a539f59a27ce17c04867",
+    "url": "/react-cursor-2hw/static/media/female.ae10377b.svg"
+  },
+  {
+    "revision": "1c57bf04602ccc1b2a746bdca55fe19c",
+    "url": "/react-cursor-2hw/static/media/male.1c57bf04.svg"
+  },
+  {
+    "revision": "8b58d9acdde1287922a306a02abc749a",
+    "url": "/react-cursor-2hw/static/media/mask.8b58d9ac.svg"
+  }
+]);
