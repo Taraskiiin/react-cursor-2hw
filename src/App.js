2d054e9b981746bce4eb6e3ed6769250d172d77a
taraskiiin <taraskravets95@gmail.com>
2020-04-06 02:19:32 +0300

    hw16reactASSfistingWithfuuuuuuusearch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Contacts from "./contacts/contacts.js";
+const contacts = [{
+  id:1,
+  firstName: "Барней",
+  lastName: "Стинсовский",
+  phone: "+380956319521",
+  gender: "male"
+},
+{
+  id:2,
+  firstName: "Робин",
+  lastName: "Щербатская",
+  phone: "+380931460123",
+  gender: "female"
+},
+{
+  id:3,
+  firstName: "Аномный",
+  lastName: "Анонимус",
+  phone: "+3806666666",
+  gender: ""
+},
+{
+  id:4,
+  firstName: "Лилия",
+  lastName: "Олдровна",
+  phone: "+380504691254",
+  gender: "female"
+},
+{
+  id:5,
+  firstName: "Маршэн",
+  lastName: "Эриксонян",
+  phone: "+380739432123",
+  gender: "male"
+},
+{
+  id:6,
+  firstName: "Теодор",
+  lastName: "Мотсбэс",
+  phone: "+380956319520",
+  gender: "male"
+}];
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Contacts contacts = {contacts} />
+    );
 }
 
 export default App;
---
+++ src/contacts/contacts.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../App.js';
+import Contact from '../contact/сontact.js';
+import './contacts.css'
+
+class Contacts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: this.props.contacts,
+            term: ""
+        };
+    }
+    searchHandler(event) {
+        this.setState({ term: event.target.value });
+    }
+    render() {
+        let filterContacts = this.state.contacts.filter(
+            (contact) => {
+               return contact.lastName.toLowerCase().indexOf(this.state.term.toLowerCase()) !== -1;
+            }
+        );
+        return (
+            <div className="hw__inner">
+                <form>
+                    <input type="text" placeholder="Search"
+                        onChange={this.searchHandler.bind(this)}
+                        value={this.state.term}
+                    />
+                </form>
+                <ul className="contacts__list">
+                    {filterContacts.map(element => {
+                        return <li key={element.id}><Contact {...element} /></li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+export default Contacts
