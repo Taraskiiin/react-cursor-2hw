0b36c627db71a960bd9b55221da9d9865dfbe524
Taraskiiin <taraskravets95@gmail.com>
2020-04-07 18:53:27 +0300

    searchUPGrade

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Contacts from "./contacts/contacts.js";
-const contacts = [{
-  id:1,
-  firstName: "Барней",
-  lastName: "Стинсовский",
-  phone: "+380956319521",
-  gender: "male"
-},
-{
-  id:2,
-  firstName: "Робин",
-  lastName: "Щербатская",
-  phone: "+380931460123",
-  gender: "female"
-},
-{
-  id:3,
-  firstName: "Аномный",
-  lastName: "Анонимус",
-  phone: "+3806666666",
-  gender: ""
-},
-{
-  id:4,
-  firstName: "Лилия",
-  lastName: "Олдровна",
-  phone: "+380504691254",
-  gender: "female"
-},
-{
-  id:5,
-  firstName: "Маршэн",
-  lastName: "Эриксонян",
-  phone: "+380739432123",
-  gender: "male"
-},
-{
-  id:6,
-  firstName: "Теодор",
-  lastName: "Мотсбэс",
-  phone: "+380956319520",
-  gender: "male"
-}];
+const contacts = [
+  {
+    id: 1,
+    firstName: "Барней",
+    lastName: "Стинсовский",
+    phone: "+380956319521",
+    gender: "male",
+  },
+  {
+    id: 2,
+    firstName: "Робин",
+    lastName: "Щербатская",
+    phone: "+380931460123",
+    gender: "female",
+  },
+  {
+    id: 3,
+    firstName: "Аномный",
+    lastName: "Анонимус",
+    phone: "+3806666666",
+    gender: "",
+  },
+  {
+    id: 4,
+    firstName: "Лилия",
+    lastName: "Олдровна",
+    phone: "+380504691254",
+    gender: "female",
+  },
+  {
+    id: 5,
+    firstName: "Маршэн",
+    lastName: "Эриксонян",
+    phone: "+380739432123",
+    gender: "male",
+  },
+  {
+    id: 6,
+    firstName: "Теодор",
+    lastName: "Мотсбэс",
+    phone: "+380956319520",
+    gender: "male",
+  },
+];
 function App() {
-    return (
-      <Contacts contacts = {contacts} />
-    );
+  return <Contacts contacts={contacts} />;
 }
 
 export default App;
--- src/contacts/contacts.js
+++ src/contacts/contacts.js
@@ -1,40 +1,48 @@
-import React from 'react';
-import '../App.js';
-import Contact from '../contact/сontact.js';
-import './contacts.css'
+import React from "react";
+import "../App.js";
+import Contact from "../contact/сontact.js";
+import "./contacts.css";
 
 class Contacts extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            contacts: this.props.contacts,
-            term: ""
-        };
-    }
-    searchHandler(event) {
-        this.setState({ term: event.target.value });
-    }
-    render() {
-        const filterContacts = this.state.contacts.filter(
-            (contact) => {
-               return contact.lastName.toLowerCase().indexOf(this.state.term.toLowerCase()) !== -1;
-            }
-        );
-        return (
-            <div className="hw__inner">
-                <form>
-                    <input type="text" placeholder="Search"
-                        onChange={this.searchHandler.bind(this)}
-                        value={this.state.term}
-                    />
-                </form>
-                <ul className="contacts__list">
-                    {filterContacts.map(element => {
-                        return <li key={element.id}><Contact {...element} /></li>
-                    })}
-                </ul>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: this.props.contacts,
+      term: "",
+    };
+  }
+  searchHandler(event) {
+    this.setState({ term: event.target.value });
+  }
+  render() {
+    const filter = this.state.contacts.filter((contact) => {
+      return (
+        (contact.lastName.toLowerCase().indexOf(this.state.term.toLowerCase()) !== -1) ||
+        (contact.firstName.toLowerCase().indexOf(this.state.term.toLowerCase()) !== -1) ||
+        (contact.phone.indexOf(this.state.term) !== -1)
+      );
+    });
+    return (
+      <div className="hw__inner">
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={this.searchHandler.bind(this)}
+            value={this.state.term}
+          />
+        </form>
+        <ul className="contacts__list">
+          {filter.map(element => {
+            return (
+              <li key={element.id}>
+                <Contact {...element} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
-export default Contacts
+export default Contacts;
